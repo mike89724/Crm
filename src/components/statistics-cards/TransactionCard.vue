f71f0e474da3812c95bbcc9d586fcd9f5f5ff0ed
abhirath14 <shubham@getnuo.com>
2020-06-18 23:42:45 +0530

    trophy icon removed and hash icon added

--- src/components/statistics-cards/TransactionCard.vue
+++ src/components/statistics-cards/TransactionCard.vue
@@ -1,12 +1,14 @@
 <template>
     <div id="transaction-card">
         <vx-card>
-            <div class="text-2xl text-left p-3">{{title}}</div>
             <div class="flex justify-between">
-                <div class="text-5xl bold text-left p-3">{{amount}}</div>
-                <div>
-                    <img class="h-20 w-20" :src="imageUrl">
-                </div>
+              <div class="text-2xl text-left p-3 w-1/2">{{title}}</div>
+              <div class="flex items-center">
+                <feather-icon icon="HashIcon"></feather-icon>
+              </div>
+            </div>
+            <div class="flex justify-between">
+              <div class="text-5xl bold text-left p-3">{{amount}}</div>
             </div>
         </vx-card>
     </div>
